a040b1ac4482a02f2b80f0d1a22ff9a0ddf582c7
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-05-09 18:52:12 -0500

    set up folder structure and added seed data

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,3 +3,12 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+// client side rendered app: react (cra)
+// database: firebase
+// react-loading-skeleton
+// styling: tailwind
+
+// folder structure
+// src
+// components, constants context, helpers, hooks, pages, lib (firebase lives here), services, styles
